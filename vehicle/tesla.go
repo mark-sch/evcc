d586f3b7584018a8789b68de21b0823892489c9b
mark-sch <mark@schmalohr.de>
2021-03-17 09:07:57 +0100

    Added api.ChargeState and api.VehicleFinishTimer

--- vehicle/tesla.go
+++ vehicle/tesla.go
@@ -6,10 +6,10 @@ import (
 	"strings"
 	"time"
 
-	"github.com/mark-sch/evcc/api"
-	"github.com/mark-sch/evcc/provider"
-	"github.com/mark-sch/evcc/util"
-	"github.com/mark-sch/evcc/util/request"
+	"github.com/andig/evcc/api"
+	"github.com/andig/evcc/provider"
+	"github.com/andig/evcc/util"
+	"github.com/andig/evcc/util/request"
 	"github.com/bogosj/tesla"
 	"golang.org/x/oauth2"
 )
