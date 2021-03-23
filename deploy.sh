c7bfecb2effa5d64b6561398eb1719e9cf48bb4c
mark-sch <mark@schmalohr.de>
2021-03-23 12:54:36 +0100

    Updates template config and deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #make
 env GOOS=linux GOARCH=arm make
-rsync --progress -e "ssh -i ~/.ssh/S5B" evcc sunny5@192.168.5.186:/home/sunny5/git/evcc/
-#rsync evcc.yaml sunny5@192.168.5.186:/home/sunny5/git/evcc/
-rsync --progress -e "ssh -i ~/.ssh/S5B" evcc.sunny5.yaml sunny5@192.168.5.186:/home/sunny5/git/evcc/
-rsync --progress -e "ssh -i ~/.ssh/S5B" build-sunny5-config.js sunny5@192.168.5.186:/home/sunny5/git/evcc/
+rsync --progress -e "ssh -i ~/.ssh/S5B" evcc sunny5@192.168.5.185:/home/sunny5/git/evcc/
+#rsync evcc.yaml sunny5@192.168.5.185:/home/sunny5/git/evcc/
+rsync --progress -e "ssh -i ~/.ssh/S5B" evcc.sunny5.yaml sunny5@192.168.5.185:/home/sunny5/git/evcc/
+rsync --progress -e "ssh -i ~/.ssh/S5B" build-sunny5-config.js sunny5@192.168.5.185:/home/sunny5/git/evcc/
