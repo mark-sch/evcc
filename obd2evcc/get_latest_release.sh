c6e1663d6437a194a0b9ffc7ffe079fe5419a59d
mark-sch <mark@schmalohr.de>
2021-07-14 12:30:10 +0200

    Enhanced obd2evcc install script

--- obd2evcc/get_latest_release.sh
+++ obd2evcc/get_latest_release.sh
@@ -9,17 +9,19 @@ function restartSmartplug {
 }
 
 function updateSmartplug {
-  MYPATH=$(basename "$pwd")
-  if [[ MYPATH="obd2evcc" ]]
+  MYPATH=$(basename "$PWD")
+  
+  if [[ "$MYPATH" == "obd2evcc" ]]
   then
     echo
   else
     mkdir obd2evcc
     cd obd2evcc
-    echo created obd2evcc directory
+    echo Created obd2evcc directory
     echo
   fi
   svn export https://github.com/mark-sch/evcc/trunk/obd2evcc ./ --force
+  npm install
 
   echo 
   echo Update complete!
@@ -28,22 +30,30 @@ function updateSmartplug {
   echo $VERSION > version.txt
   cat version.txt
   echo 
-  echo You need to restart the service for any changes to take effect.
-  echo
 
-  while true; do
-    read -p "Do you want to restart the Sunny5-Smartplug service now? " yn
-    case $yn in
-        [Yy]* ) restartSmartplug; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no. ";;
-    esac
-  done
+  if [[ "$PM2" == "sunny5smartplug" ]]
+  then
+    echo You need to restart the service for any changes to take effect.
+    echo
+
+    while true; do
+        read -p "Do you want to restart the Sunny5-Smartplug service now? " yn
+        case $yn in
+            [Yy]* ) restartSmartplug; break;;
+            [Nn]* ) break;;
+            * ) echo "Please answer yes or no. ";;
+        esac
+    done
+  else
+    echo You can search your dongles and configure vehicles by running: node build-smartplug-config.js
+    echo To start obd2evcc run: node smartplug-service.js
+    echo It is recommended to use a tool like PM2 for running obd2evcc as a background service.
+  fi
 }
 
 
 echo
-echo Smartplug-Service Update:
+echo Smartplug-Service Installation:
 echo ======================================
 echo 
 echo Installed version:
@@ -51,6 +61,7 @@ cat version.txt 2>/dev/null
 echo 
 
 VERSION=$(svn info https://github.com/mark-sch/evcc/trunk/obd2evcc 2>&1 | grep -Po 'Rev: [0-9]+' | grep -Po [0-9]+)
+PM2=$(pm2 list 2>/dev/null | grep -Po 'sunny5smartplug')
 echo Available version: 
 echo $VERSION
 echo 
